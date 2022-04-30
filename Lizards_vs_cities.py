530e865d441a6758c137cc49557219b474172f42
Jake Southall <jake321southall@aol.com>
2022-04-30 16:46:36 +0100

    Added working revive() and dying_roar() methods.

--- Lizards_vs_cities.py
+++ Lizards_vs_cities.py
@@ -1,14 +1,31 @@
 class Lizard():
-	def __init__(self, name, attack_power, belly_size, health, is_hungry = True):
+	def __init__(self, name, attack_power, belly_size, health, is_hungry = True, has_let_out_dying_roar = False, times_revived = 0):
 		self.name = name
 		self.attack_power = attack_power
 		self.belly_size = belly_size
 		self.health = health
 		self.is_hungry = is_hungry
+		self.has_let_out_dying_roar = has_let_out_dying_roar
+		self.times_revived = times_revived
+
+	def __repr__(self):
+		return f'This is {self.name}. They have {self.attack_power} attack power, {self.belly_size} belly size, and {self.health} health.'
+
+	def dying_roar(self):
+		print(f'\"ROARRR!!!\", {self.name} has less than 250 health remaining.')
+
+	def revive(self):
+		self.health += 500
+		print(f'{self.name} has revived 500 health so that its health is {self.health}.')
 
 	def attack_city(self, city):
 		if not self.is_hungry:
 			print(f"{self.name} is not hungry. {city.name} is safe.") 
+		elif self.health < 500 and self.times_revived < 1:
+				self.revive()
+				self.times_revived += 1
+				print(f'{city.name} is preparing to attack {self.name}.')
+				city.attack_lizard(self)
 		else:
 			self.belly_size -= self.attack_power/city.defence_capability
 			if self.belly_size < 0:
@@ -33,6 +50,8 @@ class City():
 		if lizard.health < 0:
 			print(f'{lizard.name} has been defeated and its organs have been donated for medical research.')
 		else:
+			if lizard.health < 250:
+				lizard.dying_roar()
 			print(f'{self.name} has removed {self.attack_power} from {lizard.name}\'s health. {lizard.name} has {lizard.health} health remaining.') 
 			print(f'{lizard.name} is preparing to attack {self.name}.')
 			lizard.attack_city(self)
@@ -40,4 +59,5 @@ class City():
 
 Godzilla = Lizard('Godzilla', 1000, 1000, 500)
 Toyko = City('Toyko', 1500, 150, 10)
+print(Godzilla)
 Godzilla.attack_city(Toyko)
